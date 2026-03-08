09c1775cb1cf895d9cbba7fe462ae3dd02187799
kwtpub <10.0.kwt@mail.ru>
2026-03-09 00:45:50 +0300

    refct(programming/lab-1): убраны функции

--- Programming/lab-1/main.cpp
+++ Programming/lab-1/main.cpp
@@ -12,172 +12,8 @@
 
 #include <fstream>  // для работы с файлами (ifstream)
 #include <iostream> // ввод/вывод
-#include <limits>   // numeric_limits
 
 using namespace std;
-
-const int MAX_LINE_LEN = 256; // максимальная длина строки
-const int MAX_WORD_LEN = 64;  // максимальная длина одного слова
-const int MAX_WORDS = 128;    // максимум слов для массива уже выведенных
-
-/*******************************************************************/
-/*                 В С П О М О Г А Т Е Л Ь Н Ы Е                 */
-/*                        Ф У Н К Ц И И                            */
-/*******************************************************************/
-
-bool WordsEqual(const char* left, const char* right)
-{
-    int i;
-
-    i = 0;
-    while (left[i] != '\0' && right[i] != '\0') {
-        if (left[i] != right[i]) {
-            return false;
-        }
-        ++i;
-    }
-
-    return left[i] == right[i];
-}
-
-void CopyWord(const char* from, char* to)
-{
-    int i;
-
-    i = 0;
-    while (from[i] != '\0' && i < MAX_WORD_LEN - 1) {
-        to[i] = from[i];
-        ++i;
-    }
-    to[i] = '\0';
-}
-
-bool ReadNextWord(const char* str, int& pos, char* word)
-{
-    int i;
-
-    while (str[pos] != '\0' && str[pos] <= ' ') {
-        ++pos;
-    }
-
-    if (str[pos] == '\0') {
-        word[0] = '\0';
-        return false;
-    }
-
-    i = 0;
-    while (str[pos] != '\0' && str[pos] > ' ') {
-        if (i < MAX_WORD_LEN - 1) {
-            word[i] = str[pos];
-            ++i;
-        }
-        ++pos;
-    }
-    word[i] = '\0';
-
-    return true;
-}
-
-bool WordExistsInLine(const char* targetWord, const char* line)
-{
-    int pos;
-    char currentWord[MAX_WORD_LEN];
-
-    pos = 0;
-    while (ReadNextWord(line, pos, currentWord)) {
-        if (WordsEqual(targetWord, currentWord)) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-bool WordAlreadyPrinted(const char* word, const char printedWords[MAX_WORDS][MAX_WORD_LEN], int printedCount)
-{
-    int i;
-
-    for (i = 0; i < printedCount; ++i) {
-        if (WordsEqual(word, printedWords[i])) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-/*******************************************************************/
-/*                 П Р О Ц Е Д У Р Ы   П О   З А Д А Н И Ю        */
-/*******************************************************************/
-
-void PrintCommonWords(const char* firstLine, const char* secondLine)
-{
-    int pos;
-    int printedCount;
-    bool hasCommonWords;
-
-    char word[MAX_WORD_LEN];
-    char printedWords[MAX_WORDS][MAX_WORD_LEN];
-
-    pos = 0;
-    printedCount = 0;
-    hasCommonWords = false;
-
-    while (ReadNextWord(firstLine, pos, word)) {
-        if (!WordAlreadyPrinted(word, printedWords, printedCount) && WordExistsInLine(word, secondLine)) {
-            cout << word << '\n';
-            hasCommonWords = true;
-
-            if (printedCount < MAX_WORDS) {
-                CopyWord(word, printedWords[printedCount]);
-                ++printedCount;
-            }
-        }
-    }
-
-    if (!hasCommonWords) {
-        cout << "(общих слов нет)" << '\n';
-    }
-}
-
-int CountSymbolInLine(const char* line, char symbol)
-{
-    int count;
-    int i;
-
-    count = 0;
-    i = 0;
-    while (line[i] != '\0') {
-        if (line[i] == symbol) {
-            ++count;
-        }
-        ++i;
-    }
-
-    return count;
-}
-
-void PrintLineWithMoreSymbol(const char* firstLine, const char* secondLine, char symbol)
-{
-    int firstCount;
-    int secondCount;
-
-    firstCount = CountSymbolInLine(firstLine, symbol);
-    secondCount = CountSymbolInLine(secondLine, symbol);
-
-    if (firstCount > secondCount) {
-        cout << "Символ '" << symbol << "' чаще встречается в первой строке: "
-             << firstCount << " против " << secondCount << "." << '\n';
-    }
-    else if (secondCount > firstCount) {
-        cout << "Символ '" << symbol << "' чаще встречается во второй строке: "
-             << secondCount << " против " << firstCount << "." << '\n';
-    }
-    else {
-        cout << "Символ '" << symbol << "' встречается одинаково в обеих строках: "
-             << firstCount << "." << '\n';
-    }
-}
 
 /*******************************************************************/
 /*                 О С Н О В Н А Я   П Р О Г Р А М М А            */
@@ -186,17 +22,44 @@ void PrintLineWithMoreSymbol(const char* firstLine, const char* secondLine, char
 int main()
 {
     // Выберите нужный набор входных данных:
-    const char* inputPath = "tests/input1.txt"; // Тест 1: общие слова есть, символ чаще в 1-й строке.
+    const int MAX_LINE_LEN = 256; // максимальная длина строки
+    const int MAX_WORD_LEN = 64;  // максимальная длина одного слова
+    const int MAX_WORDS = 128;    // максимум слов для массива уже выведенных
+    // const char* inputPath = "tests/input1.txt"; // Тест 1: общие слова есть, символ чаще в 1-й строке.
     // const char* inputPath = "tests/input2.txt"; // Тест 2: общих слов нет, символ чаще во 2-й строке.
     // const char* inputPath = "tests/input3.txt"; // Тест 3: общих слов нет, частоты символа равны.
     // const char* inputPath = "tests/input4.txt"; // Тест 4: дубли слов в первой строке.
     // const char* inputPath = "tests/input5.txt"; // Тест 5: лишние пробелы в строках.
+    // const char* inputPath = "tests/input6.txt"; // Тест 6 (ошибка): файл пустой.
+    // const char* inputPath = "tests/input7.txt"; // Тест 7 (ошибка): есть символ, но нет строк.
+    // const char* inputPath = "tests/input8.txt"; // Тест 8 (ошибка): есть символ и одна строка, вторая отсутствует.
+    // const char* inputPath = "tests/input9.txt"; // Тест 9 (ошибка): файл не существует.
 
-    // Объявление переменных
-    ifstream fin;                   // поток для чтения входного файла
-    char symbol;                    // заданный символ
-    char firstLine[MAX_LINE_LEN];   // первая строка слов
-    char secondLine[MAX_LINE_LEN];  // вторая строка слов
+    // Файл и входные данные
+    ifstream fin;                               // поток для чтения входного файла
+    char symbol;                                // заданный символ
+    char firstLine[MAX_LINE_LEN];               // первая строка слов
+    char secondLine[MAX_LINE_LEN];              // вторая строка слов
+
+    // Задание 1: общие слова
+    char word[MAX_WORD_LEN];                    // текущее слово из первой строки
+    char word2[MAX_WORD_LEN];                   // текущее слово из второй строки
+    char printedWords[MAX_WORDS][MAX_WORD_LEN]; // уже выведенные слова (для устранения дублей)
+    int printedCount;                           // количество уже выведенных слов
+    bool hasCommonWords;                        // флаг наличия общих слов
+    bool alreadyPrinted;                        // флаг: слово уже выводилось
+    bool foundInSecond;                         // флаг: слово найдено во второй строке
+
+    // Задание 2: частота символа
+    int firstCount;                             // количество вхождений символа в первой строке
+    int secondCount;                            // количество вхождений символа во второй строке
+
+    // Вспомогательные индексы
+    int pos1;                                   // позиция в первой строке
+    int pos2;                                   // позиция во второй строке
+    int wi;                                     // индекс при записи символов в слово
+    int i;                                      // счётчик цикла
+    int ci;                                     // индекс при посимвольном сравнении
 
     // Открытие входного файла
     fin.open(inputPath);
@@ -223,7 +86,7 @@ int main()
     }
 
     // Переход на следующую строку перед getline
-    fin.ignore(numeric_limits<streamsize>::max(), '\n');
+    fin.ignore('\n');
 
     // Чтение первой строки
     if (!fin.getline(firstLine, MAX_LINE_LEN)) {
@@ -258,13 +121,150 @@ int main()
     cout << "Первая строка: " << firstLine << '\n';
     cout << "Вторая строка: " << secondLine << '\n';
 
-    // 1. Печать слов, встречающихся в обеих строках
-    cout << "\nСлова, встречающиеся в обеих строках:" << '\n';
-    PrintCommonWords(firstLine, secondLine);
+    /*******************************************************************/
+    /* 1. Слова, встречающиеся в обеих строках                         */
+    /*******************************************************************/
 
-    // 2. Выявление строки, в которой символ встречается чаще
+    cout << "\nСлова, встречающиеся в обеих строках:" << '\n';
+
+    pos1 = 0;
+    printedCount = 0;
+    hasCommonWords = false;
+
+    // Перебираем слова первой строки
+    while (true) {
+        // Пропускаем пробелы
+        while (firstLine[pos1] != '\0' && firstLine[pos1] == ' ') {
+            ++pos1;
+        }
+        if (firstLine[pos1] == '\0') {
+            break;
+        }
+
+        // Читаем слово из первой строки
+        wi = 0;
+        while (firstLine[pos1] != '\0' && firstLine[pos1] != ' ') {
+            if (wi < MAX_WORD_LEN - 1) {
+                word[wi] = firstLine[pos1];
+                ++wi;
+            }
+            ++pos1;
+        }
+        word[wi] = '\0';
+
+        // Проверяем, не выводили ли это слово уже
+        alreadyPrinted = false;
+        for (i = 0; i < printedCount; ++i) {
+            ci = 0;
+            while (word[ci] != '\0' && printedWords[i][ci] != '\0') {
+                if (word[ci] != printedWords[i][ci]) {
+                    break;
+                }
+                ++ci;
+            }
+            if (word[ci] == printedWords[i][ci]) {
+                alreadyPrinted = true;
+                break;
+            }
+        }
+        if (alreadyPrinted) {
+            continue;
+        }
+
+        // Ищем слово во второй строке
+        foundInSecond = false;
+        pos2 = 0;
+
+        while (true) {
+            while (secondLine[pos2] != '\0' && secondLine[pos2] == ' ') {
+                ++pos2;
+            }
+            if (secondLine[pos2] == '\0') {
+                break;
+            }
+
+            wi = 0;
+            while (secondLine[pos2] != '\0' && secondLine[pos2] != ' ') {
+                if (wi < MAX_WORD_LEN - 1) {
+                    word2[wi] = secondLine[pos2];
+                    ++wi;
+                }
+                ++pos2;
+            }
+            word2[wi] = '\0';
+
+            // Сравниваем word и word2
+            ci = 0;
+            while (word[ci] != '\0' && word2[ci] != '\0') {
+                if (word[ci] != word2[ci]) {
+                    break;
+                }
+                ++ci;
+            }
+            if (word[ci] == word2[ci]) {
+                foundInSecond = true;
+                break;
+            }
+        }
+
+        if (foundInSecond) {
+            cout << word << '\n';
+            hasCommonWords = true;
+
+            // Запоминаем слово чтобы не выводить дубли
+            if (printedCount < MAX_WORDS) {
+                ci = 0;
+                while (word[ci] != '\0' && ci < MAX_WORD_LEN - 1) {
+                    printedWords[printedCount][ci] = word[ci];
+                    ++ci;
+                }
+                printedWords[printedCount][ci] = '\0';
+                ++printedCount;
+            }
+        }
+    }
+
+    if (!hasCommonWords) {
+        cout << "(общих слов нет)" << '\n';
+    }
+
+    /*******************************************************************/
+    /* 2. Строка, в которой символ встречается чаще                    */
+    /*******************************************************************/
+
     cout << "\nСравнение частоты символа:" << '\n';
-    PrintLineWithMoreSymbol(firstLine, secondLine, symbol);
+
+    firstCount = 0;
+    secondCount = 0;
+
+    i = 0;
+    while (firstLine[i] != '\0') {
+        if (firstLine[i] == symbol) {
+            ++firstCount;
+        }
+        ++i;
+    }
+
+    i = 0;
+    while (secondLine[i] != '\0') {
+        if (secondLine[i] == symbol) {
+            ++secondCount;
+        }
+        ++i;
+    }
+
+    if (firstCount > secondCount) {
+        cout << "Символ '" << symbol << "' чаще встречается в первой строке: "
+             << firstCount << " против " << secondCount << "." << '\n';
+    }
+    else if (secondCount > firstCount) {
+        cout << "Символ '" << symbol << "' чаще встречается во второй строке: "
+             << secondCount << " против " << firstCount << "." << '\n';
+    }
+    else {
+        cout << "Символ '" << symbol << "' встречается одинаково в обеих строках: "
+             << firstCount << "." << '\n';
+    }
 
     return 0;
 }
